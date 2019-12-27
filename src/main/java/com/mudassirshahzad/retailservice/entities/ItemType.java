a36659edb5eca1c3d098ca4da78cf30ecf574eec
Mudassir Shahzad <mudassir.shahzad@dnata.com>
2019-12-27 23:50:46 +0400

    Repackaged

---
+++ src/main/java/com/mudassirshahzad/retailservice/entities/ItemType.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package com.mudassirshahzad.retailservice.entities;
+
+/**
+ * @author Mudassir Shahzad Dec 27, 2019
+ */
+public enum ItemType {
+	GROCERY, ELECTRONICS, KITCHEN_ITEMS
+}
