4f08bce35b901f69baeb324436ca57b3a1f72476
Mudassir Shahzad <mudassir.shahzad@dnata.com>
2019-12-27 23:21:07 +0400

    Added main application test.

--- retailservice/src/test/java/com/mudassirshahzad/retailservice/RetailApplicationTest.java
+++ retailservice/src/test/java/com/mudassirshahzad/retailservice/RetailApplicationTest.java
@@ -2,16 +2,21 @@ package com.mudassirshahzad.retailservice;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
-class RetailApplicationTests {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@DisplayName("Application Unit Tests")
+class RetailApplicationTest {
 
 	@Autowired
 	MockMvc mockMvc;
